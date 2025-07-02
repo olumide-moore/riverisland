3d564a8ab3e4869f2902af4ddf4bc61297680cc6
Mide Moore <olumide.moore2817@gmail.com>
2025-07-02 16:31:14 +0100

    Created a minimal homepage for a start

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import { GenderCategories } from "../components/GenderCategory/GenderCategories";
+
+const HomePage = () => {
+  return (
+    // <div className="py-8 container mx-auto flex flex-col flex-wrap justify-center gap-10">
+    <div className="py-0 flex flex-col flex-wrap justify-center gap-8">
+      
+      <GenderCategories />
+    </div>
+  );
+};
+
+export default HomePage;
