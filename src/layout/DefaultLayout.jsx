732ee750d3858c90e3ab24654653444a2c612002
Mide Moore <olumide.moore2817@gmail.com>
2025-07-02 16:33:43 +0100

    Removed search for now

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 import Sidebar from "../components/sidebar";
 import Footer from "../components/Footer";
 
-const DefaultLayout = ({ setSearchText, children }) => {
+const DefaultLayout = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
@@ -11,7 +11,7 @@ const DefaultLayout = ({ setSearchText, children }) => {
         {/* <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} /> */}
         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
         {/* <div className="flex flex-1 gap-0 flex-col overflow-y-auto overflow-x-hidden"> */}
-          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} setSearchText={setSearchText} />
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
           <main>
             <div className="min-h-screen bg-white">{children}</div>
           </main>
