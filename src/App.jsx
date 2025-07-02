c566dc124c4d28333caa8fbdcfeafe42dce0d60e
Mide Moore <olumide.moore2817@gmail.com>
2025-07-02 16:36:36 +0100

    Added routes for auths, cart, wishlist, categories, products

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,64 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import DefaultLayout from "./layout/DefaultLayout";
 import { useState } from "react";
-import Product from "./pages/Product";
-import ProductsList from "./pages/ProductsList";
-import { GetEmail } from "./pages/Auth/GetEmail";
-import { Login } from "./pages/Auth/Login";
-import { Signup } from "./pages/Auth/Signup";
+import DefaultLayout from "./layout/DefaultLayout";
+import HomePage from "./pages/HomePage";
+import CategoriesPage from "./pages/CategoriesPage";
+import ProductsPage from "./pages/ProductsPage";
+import ProductItemPage from "./pages/ProductItemPage";
+import GetEmailPage from "./pages/Auth/GetEmailPage";
+import LoginPage from "./pages/Auth/LoginPage";
+import SignupPage from "./pages/Auth/SignupPage";
+import WishListPage from "./pages/WishListPage";
+import AccountOverview from "./pages/Account/AccountOverview";
+import RequireNoAuth from "./components/account/RequireNoAuth";
+import Account from "./pages/Account/Account";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import CartPage from "./pages/CartPage";
 
 export default function App() {
-  const [searchText, setSearchText] = useState("");
   return (
-    <DefaultLayout setSearchText={setSearchText}>
+    <Provider store={store}>
+      <DefaultLayout>
         <Routes>
-          <Route  index path="/" element={<Home searchText={searchText} />} />
-          <Route path="/productslist/:id" element={<ProductsList />} />
-          <Route path="/product/:id" element={<Product />} />
-          <Route path="/getemail" element={<GetEmail />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
+          <Route
+            index
+            path="/"
+            element={<HomePage />}
+          />
+          <Route path="/:gendercategoryid" element={<CategoriesPage />} />
+          <Route path="/products/:categoryid" element={<ProductsPage />} />
+          <Route path="/product/:name" element={<ProductItemPage />} />
+          <Route path="/wishlist" element={<WishListPage />} />
+          <Route path="/cart" element={<CartPage />} />
+
+          <Route
+            path="/getemail"
+            element={
+              <RequireNoAuth>
+                <GetEmailPage />
+              </RequireNoAuth>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RequireNoAuth>
+                <LoginPage />
+              </RequireNoAuth>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <RequireNoAuth>
+                <SignupPage />
+              </RequireNoAuth>
+            }
+          />
+          <Route path="/myaccount" element={<Account />} />
         </Routes>
-    </DefaultLayout>
+      </DefaultLayout>
+    </Provider>
   );
 }
