eb10a1724bbed128b19a79e0237ce1ed8af6a6ce
Mide Moore <olumide.moore2817@gmail.com>
2025-07-07 15:40:27 +0100

    Added context api providers for wishlist and cart

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./contexts/CartContext.jsx";
+import { WishlistProvider } from "./contexts/WishlistContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <CartProvider>
-        <App />
-      </CartProvider>
+      <WishlistProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </WishlistProvider>
     </BrowserRouter>
   </StrictMode>
 );
