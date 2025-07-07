089ddb46bb00f26fbc1af4d48d76c2af52f49060
Mide Moore <olumide.moore2817@gmail.com>
2025-07-07 15:41:26 +0100

    Added wishlist and product api in store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cartApi } from "../features/cartSlice";
+import { wishlistApi } from "../features/wishlistSlice";
+import { productApi } from "../features/productSlice";
 
 export const store = configureStore({
   reducer: {
+    [productApi.reducerPath]: productApi.reducer,
     [cartApi.reducerPath]: cartApi.reducer,
+    [wishlistApi.reducerPath]: wishlistApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(cartApi.middleware),
+    getDefaultMiddleware().concat(productApi.middleware, cartApi.middleware, wishlistApi.middleware),
 });
