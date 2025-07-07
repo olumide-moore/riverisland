a66a5eee53894dcaba2df637ed8a4019f0ba1f17
Mide Moore <olumide.moore2817@gmail.com>
2025-07-07 19:45:48 +0100

    Added div for when loading

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -85,9 +85,16 @@ const CartPage = () => {
     getCartItems();
   }, [serverCart, localCart]);
 
+  if (cartLoading || productsLoading) {
+    return (
+      <div className="fixed inset-0 bg-white bg-opacity-90 z-50 flex items-center justify-center">
+        <div className="w-12 h-12 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+      </div>
+    );
+  }
   return (
+    
     <div className="mx-14 mt-8 mb-20">
-      {cartLoading && <p>Loading...</p>}
       <h2 className="text-3xl font-open-sans font-medium text-center">Bag</h2>
       {cartItems.length > 0 ? (
         <div className="lg:flex lg:justify-center lg:gap-20">
