21ba8ef3ccdac9eb74d1309a3efce582eb3a8be1
Mide Moore <olumide.moore2817@gmail.com>
2025-07-07 18:05:40 +0100

    Changed background color of gender categories

--- src/components/gendercategory/GenderCategory.jsx
+++ src/components/gendercategory/GenderCategory.jsx
@@ -15,7 +15,7 @@ const GenderCategory = ({gendercategory}) => {
         // className="w-[400px] h-[400px] object-cover"
             />
         {/* <h2 className="text-4xl font-medium underline mt-2 tracking-wide absolute bottom-1/2 left-1/4 text-white">{gendercategory?.name}</h2> */}
-        <h2 className="text-4xl font-medium mt-2 tracking-wide absolute bottom-2 left-1/4 p-2 rounded-lg text-white bg-gray-600">{gendercategory?.name}</h2>
+        <h2 className="text-4xl font-medium mt-2 tracking-wide absolute bottom-2 left-1/4 p-2 rounded-sm text-white bg-gray-900">{gendercategory?.name}</h2>
       </div>
     </Link>
   );
