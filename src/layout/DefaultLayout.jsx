5d6808f06052dd97e86c6184e9433a6d57f15989
Mide Moore <olumide.moore2817@gmail.com>
2025-07-07 19:39:17 +0100

    Removed previous sidebar

--- src/layout/DefaultLayout.jsx
+++ src/layout/DefaultLayout.jsx
@@ -1,17 +1,12 @@
-import { useState } from "react";
 import Header from "../components/Header";
-import Sidebar from "../components/sidebar";
 import Footer from "../components/Footer";
 
 const DefaultLayout = ({ children }) => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
       <div className="mx-auto flex h-screen">
-        {/* <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} /> */}
         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-        {/* <div className="flex flex-1 gap-0 flex-col overflow-y-auto overflow-x-hidden"> */}
-          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+          <Header/>
           <main>
             <div className="min-h-screen bg-white">{children}</div>
           </main>
