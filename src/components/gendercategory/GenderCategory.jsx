ce5f5b58d64085ad7ba3e6c69b241e063e0ee076
Mide Moore <olumide.moore2817@gmail.com>
2025-07-09 12:24:26 +0100

    Adjusted header

--- src/components/gendercategory/GenderCategory.jsx
+++ src/components/gendercategory/GenderCategory.jsx
@@ -15,7 +15,8 @@ const GenderCategory = ({gendercategory}) => {
         // className="w-[400px] h-[400px] object-cover"
             />
         {/* <h2 className="text-4xl font-medium underline mt-2 tracking-wide absolute bottom-1/2 left-1/4 text-white">{gendercategory?.name}</h2> */}
-        <h2 className="text-4xl font-medium mt-2 tracking-wide absolute bottom-2 left-1/4 p-2 rounded-sm text-white bg-gray-900">{gendercategory?.name}</h2>
+        {/* <h2 className="text-4xl font-medium mt-2 tracking-wide absolute bottom-2 left-1/4 p-2 rounded-sm text-white bg-gray-900">{gendercategory?.name}</h2> */}
+        <h2 className={`text-4xl font-medium mt-2 tracking-wide absolute bottom-20 left-1/3 p-2 rounded-sm underline ${gendercategory?.name=="Women" ? 'text-gray-900' : 'text-white'}`}>{gendercategory?.name}</h2>
       </div>
     </Link>
   );
